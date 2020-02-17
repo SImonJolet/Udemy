5f39a33816ad000499173461fa3b80d08db0ad92
Jolet Simon <jolet.simon@yahoo.com>
2020-02-17 16:58:23 +0100

    First recipes output

--- chap4/src/app/recipes/recipes.page.ts
+++ chap4/src/app/recipes/recipes.page.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesPage implements OnInit {
 
+  recipes: Recipe[] = [
+    {
+      id:'r1',
+      title: 'Schnitzel', 
+      imageUrl: 'https://www.foodiecrush.com/wp-content/uploads/2018/10/Pork-Schnitzel-foodiecrush.com-016-683x1024.jpg',
+      ingredients: ['veau, chapelure, oeufs, farine, pomme de terre à frite']
+    },
+    {
+      id:'r2',
+      title: 'Spaghetti', 
+      imageUrl: 'https://assets.afcdn.com/recipe/20180326/78158_w1024h768c1cx2736cy1824cxt0cyt0cxb5472cyb3648.jpg',
+      ingredients: ['pate, sauce']
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
