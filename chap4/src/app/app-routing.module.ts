73216eb5fdf5267f060943a590f71e0dfaaf2bf4
Jolet Simon <jolet.simon@yahoo.com>
2020-02-19 09:35:06 +0100

    Routing

--- chap4/src/app/app-routing.module.ts
+++ chap4/src/app/app-routing.module.ts
@@ -5,11 +5,16 @@ const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   {
     path: 'recipes',
-    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
-  },
-  {
-    path: 'recipe-detail',
-    loadChildren: () => import('./recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule)
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+      },
+      {
+        path: ':recipesId',
+        loadChildren: () => import('./recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule)
+      }
+    ]
   },
 ];
 
