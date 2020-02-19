ec6cc4b1678775faa55d410821f9aada67ffce3d
Jolet Simon <jolet.simon@yahoo.com>
2020-02-19 14:38:47 +0100

    Delete

--- chap4/src/app/recipe-detail/recipe-detail.page.ts
+++ chap4/src/app/recipe-detail/recipe-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from '../service/recipes.service';
 import { Recipe } from '../recipes/recipes.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,13 +15,14 @@ export class RecipeDetailPage implements OnInit {
   constructor(
     private activitedRoute: ActivatedRoute,
     private recipesService: RecipesService,
-    private router: Router
+    private router: Router,
+    private alertCtrl: AlertController
   ) { }
 
   ngOnInit() {
     this.activitedRoute.paramMap.subscribe(paramMap => {
       if(!paramMap.has('recipeId')){
-        //redirect
+        this.router.navigate(['/recipes']);
         return;
       }else {
         const recipeId = paramMap.get('recipeId');
@@ -32,7 +34,25 @@ export class RecipeDetailPage implements OnInit {
   }
 
   onDeleteRecipe(){
-    this.recipesService.deteleRecipe(this.loadedRecipe.id);
-    this.router.navigate(['/recipes']);
+    this.alertCtrl.create({
+      header: 'Êtes-vous sûr ??',
+      message: 'Vraiment vraiment vraiment sûr ?? Pas de regrets ?',
+      buttons: [
+        {
+          text:`Noooope, je change d'avis, t'as bien fait de demander`,
+          role:'cancel'
+        },
+        {
+          text: 'Supprimer je te dis !!',
+          handler: ()=> {
+            this.recipesService.deteleRecipe(this.loadedRecipe.id);
+            this.router.navigate(['/recipes']);
+          }
+        }
+    ]
+    })
+   .then(alertEl => {
+     alertEl.present();
+   })
   }
 }
