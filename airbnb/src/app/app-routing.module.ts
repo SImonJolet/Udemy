60306525c2cc545235070b3e842bf72def92854e
Jolet Simon <jolet.simon@yahoo.com>
2020-02-21 15:39:04 +0100

    ?

--- airbnb/src/app/app-routing.module.ts
+++ airbnb/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'places', pathMatch: 'full' },
+  { path: '', redirectTo: 'places/tabs', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
   { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' }
