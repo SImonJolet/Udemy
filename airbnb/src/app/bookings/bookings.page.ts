05ef9da665bc051da5ecfffbe346f73a4e070eab
Jolet Simon <jolet.simon@yahoo.com>
2020-02-24 14:23:10 +0100

    Booking delete slinding list

--- airbnb/src/app/bookings/bookings.page.ts
+++ airbnb/src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from './bookings.service';
+import { Booking } from './bookings.model';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-bookings',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
+  loadedBookings: Booking[]
 
-  constructor() { }
+  constructor(
+    private bookingService : BookingService
+  ) { }
 
   ngOnInit() {
+
+    this.loadedBookings = this.bookingService.bookings;
+  }
+  onCancelBooking(offerId: string, slidingBooking: IonItemSliding) {  
+    slidingBooking.close();
+
   }
 
 }
--- airbnb/src/app/bookings/bookings.service.ts
+++ airbnb/src/app/bookings/bookings.service.ts
@@ -14,6 +14,7 @@ export class BookingService {
     ];
 
     get bookings() {
+        
         return [...this._bookings];
     }
 }
