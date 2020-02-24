96a21e5a9777d5b5a62faa40fe7e63169f1e7194
Jolet Simon <jolet.simon@yahoo.com>
2020-02-24 14:22:27 +0100

    Fix typo name

--- airbnb/src/app/places.service.ts
+++ airbnb/src/app/places.service.ts
@@ -10,21 +10,21 @@ export class PlacesService {
 
   private _places : Place[]= [
     new Place(
-      'o1',
+      'p1',
       'Paris',
       'Un magnifique trois pièces avec vue sur la tour Effeil',
       'https://q-xx.bstatic.com/images/hotel/max1024x768/155/155497561.jpg',
       156
     ),
     new Place(
-      'o2',
+      'p2',
       'Venise',
       'Appartement au 4e étage, très lumineux',
       'https://img-4.linternaute.com/XgC-fYZPQGbSmTLoO4e_GKd3DaU=/660x366/smart/64f33616425f4532a7d864613333af12/ccmcms-linternaute/10924262.jpg',
       130
     ),
     new Place(
-      'o3',
+      'p3',
       'Berlin',
       'Loft moderne, proche du centre',
       'https://media-cdn.tripadvisor.com/media/photo-s/14/4b/16/ed/brilliant-apartments.jpg',
