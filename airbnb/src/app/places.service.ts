53b616b98c456a4c4c2a0e3886df98afd3ad0cb9
Jolet Simon <jolet.simon@yahoo.com>
2020-02-25 17:42:03 +0100

    addPlace() + rxjs

--- airbnb/src/app/places.service.ts
+++ airbnb/src/app/places.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import {Place} from './places/place.model';
+import { AuthService } from './auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
- 
 
-  private _places : Place[]= [
+  private _places = new BehaviorSubject<Place[]>([
     new Place(
       'p1',
       'Paris',
@@ -16,7 +18,8 @@ export class PlacesService {
       'https://q-xx.bstatic.com/images/hotel/max1024x768/155/155497561.jpg',
       156,
       new Date('2019-01-01'),
-      new Date ('2019-12-31')
+      new Date ('2019-12-31'),
+      '1'
     ),
     new Place(
       'p2',
@@ -25,7 +28,8 @@ export class PlacesService {
       'https://img-4.linternaute.com/XgC-fYZPQGbSmTLoO4e_GKd3DaU=/660x366/smart/64f33616425f4532a7d864613333af12/ccmcms-linternaute/10924262.jpg',
       130,
       new Date('2019-01-01'),
-      new Date ('2019-12-31')
+      new Date ('2019-12-31'),
+      '2'
     ),
     new Place(
       'p3',
@@ -34,18 +38,42 @@ export class PlacesService {
       'https://media-cdn.tripadvisor.com/media/photo-s/14/4b/16/ed/brilliant-apartments.jpg',
       99,
       new Date('2019-01-01'),
-      new Date ('2019-12-31')
+      new Date ('2019-12-31'),
+      '3'
     )
-  ]
+  ])
 
   get places() {
-    return [...this._places];
+    return  this._places.asObservable();
   }
 
-  constructor() {}
+  constructor(
+    private authService: AuthService
+  ) {}
 
   getPlaces(id: string) {
     return {...this._places.find(p => p.id === id)};
   }
   
+  addPlace(
+    title: string,
+    description: string,
+    price: number,
+    dateFrom: Date,
+    dateTo: Date
+    ){
+      const newPlace= new Place(Math.random().toString(), 
+      title, 
+      description,
+      'https://media-cdn.tripadvisor.com/media/photo-s/14/4b/16/ed/brilliant-apartments.jpg',
+      price,
+      dateFrom,
+      dateTo,
+      this.authService.userId
+      );
+
+      this.places.pipe(take(1)).subscribe(places => {
+        this._places.next(places.concat(newPlace));
+      })
+    }
 }
