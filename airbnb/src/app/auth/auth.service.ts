d0392f3196d350c40f756a47ad67fb481ab4a594
Jolet Simon <jolet.simon@yahoo.com>
2020-02-25 11:36:43 +0100

    isAuth=> false

--- airbnb/src/app/auth/auth.service.ts
+++ airbnb/src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private _userIsAuthenticted = true;
+  private _userIsAuthenticted = false;
 
   get userIsAuthenticated() {
     return this._userIsAuthenticted;
